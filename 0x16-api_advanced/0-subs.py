9967b89970d0b93143ffde64c0e66bb430c8696b
Hisham Balboul <hishamsabry_97@hotmail.com>
2024-02-13 03:40:10 -0800

    adv API Tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """return the number of subscribers from a subreddit"""
-    path = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(path, allow_redirects=False, headers=headers)
-    if response.status_code in (302, 404):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    json = response.json()
-    return json.get('data').get('subscribers')
+    results = response.json().get("data")
+    return results.get("subscribers")
