422e6901807b88fe00e101a2acd891be5c4923f8
Hisham Balboul <hishamsabry_97@hotmail.com>
2024-02-13 03:18:50 -0800

    adv API Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return the number of subscribers from a subreddit"""
+    path = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(path, allow_redirects=False, headers=headers)
+    if response.status_code in (302, 404):
+        return 0
+
+    json = response.json()
+    return json.get('data').get('subscribers')
